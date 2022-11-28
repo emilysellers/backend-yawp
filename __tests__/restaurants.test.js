dbc2441da0583ed0b99464a961d836a693993231
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-28 15:11:39 -0800

    GET /restaurants/:id returns one restaurant, passing test

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -10,6 +10,19 @@ describe('restaurant routes', () => {
   afterAll(() => {
     pool.end();
   });
+  it('GET /api/v1/restaurants/:restId returns one existing restaurant', async () => {
+    const resp = await request(app).get('/api/v1/restaurants/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: '1',
+      name: "Pip's Original",
+      cuisine: 'American',
+      cost: 1,
+      image:
+        'https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg',
+      website: 'http://www.PipsOriginal.com',
+    });
+  });
 
   it('GET /api/v1/restaurants shows list of all restaurants', async () => {
     const resp = await request(app).get('/api/v1/restaurants');
