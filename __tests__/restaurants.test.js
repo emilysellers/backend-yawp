1d956dd572a867fbbfb4b0b4e8a85060e02e05cc
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-28 15:00:31 -0800

    GET /restaurants lists all restaurants, passing test

---
+++ __tests__/restaurants.test.js
@@ -0,0 +1,28 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('restaurant routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('GET /api/v1/restaurants shows list of all restaurants', async () => {
+    const resp = await request(app).get('/api/v1/restaurants');
+    expect(resp.status).toBe(200);
+    // expect(resp.body).toMatchInlineSnapshot({});
+    expect(resp.body[0]).toEqual({
+      id: '1',
+      name: "Pip's Original",
+      cuisine: 'American',
+      cost: 1,
+      image:
+        'https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg',
+      website: 'http://www.PipsOriginal.com',
+    });
+  });
+});
